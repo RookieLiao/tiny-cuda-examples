8262fa6dd8fa084c1910a5acc0ce24bf706117c0
sherlock <51414160797@qq.com>
2025-01-31 20:33:32 +0800

    add profile code for softmax

--- 100-days-of-cuda/day01/test_softmax.py
+++ 100-days-of-cuda/day01/test_softmax.py
@@ -2,7 +2,10 @@ import torch
 
 from softmax_cuda import softmax_kernel_launcher
 
-x = torch.rand(2048, 8192, dtype=torch.float32, device="cuda")
+rows = 1024
+cols = 8192
+
+x = torch.rand(rows, cols, dtype=torch.float32, device="cuda")
 y = torch.empty_like(x)
 
 softmax_kernel_launcher(x, y)
@@ -10,3 +13,23 @@ softmax_kernel_launcher(x, y)
 z = torch.nn.functional.softmax(x, dim=1)
 
 assert torch.allclose(y, z, atol=1e-5), "Softmax kernel is not correct"
+
+steps = 10
+start_events = [torch.cuda.Event(enable_timing=True) for _ in range(steps)]
+end_events = [torch.cuda.Event(enable_timing=True) for _ in range(steps)]
+
+# Warm up
+for _ in range(10):
+    _ = torch.nn.functional.softmax(x, dim=-1)
+    # softmax_kernel_launcher(x, y)
+
+for i in range(steps):
+    start_events[i].record()
+    _ = torch.nn.functional.softmax(x, dim=-1)
+    # softmax_kernel_launcher(x, y)
+    end_events[i].record()
+
+torch.cuda.synchronize()
+times = [start_events[i].elapsed_time(end_events[i]) for i in range(steps)]
+print(f"Softmax computation time (average): {sum(times) / steps} ms")
+
--- 100-days-of-cuda/day02/test_warp_softmax.py
+++ 100-days-of-cuda/day02/test_warp_softmax.py
@@ -19,3 +19,26 @@ z = torch.nn.functional.softmax(x, dim=1)
 assert torch.allclose(y, z, atol=1e-5), "Warp Softmax kernel is not correct"
 
 print("Test passed!")
+
+
+steps = 10
+start_events = [torch.cuda.Event(enable_timing=True) for _ in range(steps)]
+end_events = [torch.cuda.Event(enable_timing=True) for _ in range(steps)]
+
+# Initialize the matrix
+x = torch.rand(rows, cols, device="cuda", dtype=torch.float32)
+
+# Warm up
+for _ in range(10):
+    # _ = torch.nn.functional.softmax(x, dim=-1)
+    warp_softmax_launcher(x, y)
+
+for i in range(steps):
+    start_events[i].record()
+    # _ = torch.nn.functional.softmax(x, dim=-1)
+    warp_softmax_launcher(x, y)
+    end_events[i].record()
+
+torch.cuda.synchronize()
+times = [start_events[i].elapsed_time(end_events[i]) for i in range(steps)]
+print(f"Softmax computation time (average): {sum(times) / steps} ms")
