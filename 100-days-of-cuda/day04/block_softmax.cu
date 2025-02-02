c827a24824edb268ef88554d5368eec60a170e5e
sherlock <51414160797@qq.com>
2025-02-02 17:48:15 +0800

    finish block reduce kernel

---
+++ 100-days-of-cuda/day04/block_softmax.cu
@@ -0,0 +1,142 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+inline __device__ float warpReduceMax(float *val, int thread_group = 32) {
+#pragma unroll
+  for (int lane_mask = thread_group / 2; lane_mask > 0; lane_mask >>= 1) {
+    // perform warp shuffle using lane_id from 0 to 31 which is global_id %
+    // warp_size
+    val[0] = fmaxf(
+        val[0], __shfl_xor_sync(0xFFFFFFFF, val[0], lane_mask, thread_group));
+  }
+  return 0.0f;
+}
+
+inline __device__ float warpReduceSum(float *val, int thread_group = 32) {
+#pragma unroll
+  for (int lane_mask = thread_group / 2; lane_mask > 0; lane_mask >>= 1) {
+    val[0] += __shfl_xor_sync(0xFFFFFFFF, val[0], lane_mask, thread_group);
+  }
+  return 0.0f;
+}
+
+inline __device__ float blockReduceMax(float *val, int thread_group = 32) {
+  __shared__ float shared_val[33];
+  int lane = threadIdx.x & 0x1f; // threadIdx.x % warp_size
+  int wid = threadIdx.x >> 5;    // threadIdx.x / warp_size
+
+  warpReduceMax(val, thread_group);
+  int tid = threadIdx.x;
+  if (lane == 0) {
+    shared_val[wid] = val[0];
+  }
+  __syncthreads();
+
+  val[0] = tid < (blockDim.x / 32.f) ? shared_val[tid] : -INFINITY;
+
+  // only perform on the first warp
+  if (wid == 0) {
+    warpReduceMax(val, 32);
+  }
+  return 0.0f;
+}
+
+inline __device__ float blockReduceSum(float *val, int thread_group = 32) {
+  __shared__ float shared_val[33];
+  int lane = threadIdx.x & 0x1f; // threadIdx.x % warp_size
+  int wid = threadIdx.x >> 5;    // threadIdx.x / warp_size
+
+  warpReduceSum(val, thread_group);
+  int tid = threadIdx.x;
+  if (lane == 0) {
+    shared_val[wid] = val[0];
+  }
+  __syncthreads();
+
+  val[0] = tid < (blockDim.x / 32.f) ? shared_val[tid] : 0.0f;
+
+  if (wid == 0) {
+    warpReduceSum(val, 32);
+  }
+  return 0.0f;
+}
+
+__global__ void block_softmax_kernel(const float *input, float *output,
+                                     size_t m, size_t n) {
+  int row_offset = blockIdx.x;
+
+  if (row_offset >= m)
+    return;
+
+  const float *row_input = input + row_offset * n;
+  float *row_output = output + row_offset * n;
+
+  float local_max[1] = {-INFINITY};
+  for (int i = 0; i < n; i += blockDim.x) {
+    int col_idx = i + threadIdx.x;
+    if (col_idx < n) {
+      local_max[0] = fmax(local_max[0], row_input[col_idx]);
+    }
+  }
+  blockReduceMax(local_max, 1);
+
+  __shared__ float shared_val[2];
+  if (threadIdx.x == 0) {
+    shared_val[0] = local_max[0];
+  }
+  __syncthreads();
+
+  float local_sum[1] = {0.0f};
+  for (int i = 0; i < n; i += blockDim.x) {
+    int col_idx = i + threadIdx.x;
+    if (col_idx < n) {
+      local_sum[0] += expf(row_input[col_idx] - shared_val[0]);
+    }
+  }
+  blockReduceSum(local_sum, 1);
+
+  if (threadIdx.x == 0) {
+    shared_val[1] = local_sum[0];
+  }
+  __syncthreads();
+
+  for (int i = 0; i < n; i += blockDim.x) {
+    int col_idx = i + threadIdx.x;
+    if (col_idx < n) {
+      row_output[col_idx] =
+          expf(row_input[col_idx] - shared_val[0]) / shared_val[1];
+    }
+  }
+}
+
+int main() {
+  int m = 1;
+  int n = 1024;
+
+  float *input_h = (float *)malloc(m * n * sizeof(float));
+  float *output_h = (float *)malloc(m * n * sizeof(float));
+
+  // initialize input
+  for (int i = 0; i < m * n; i++) {
+    input_h[i] = 1.0f;
+  }
+
+  float *input_d;
+  cudaMalloc(&input_d, m * n * sizeof(float));
+  float *output_d;
+  cudaMalloc(&output_d, m * n * sizeof(float));
+
+  cudaMemcpy(input_d, input_h, m * n * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 grid_size(m);
+  dim3 block_size(512);
+
+  block_softmax_kernel<<<grid_size, block_size>>>(input_d, output_d, m, n);
+
+  cudaMemcpy(output_h, output_d, m * n * sizeof(float), cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < m * n; i++) {
+    printf("%f ", output_h[i]);
+  }
+  printf("\n");
+}
---
+++ 100-days-of-cuda/day04/note.md
@@ -0,0 +1,9 @@
+
+
+Ideally, reducing among wrap is fastest. However, we need local registers to store inputs as each thread needs to store K/32*pack_size*sizeof(float) bytes.
+
+Q: Why need to store `K/32*pack_size*sizeof(float)` bytes?
+A: the GPU compiler (PTXAS) does register allocation at compile time. It decides how many registers a kernel needs at peak usage. You don’t manually free them in code like you might do with a CPU stack variable.
+
+Peak usage: If the kernel’s code requires, say, 48 registers at its most demanding moment (e.g., right after loading multiple values), then 48 registers are allocated for that thread for the entire kernel lifetime.
+If you ask the compiler to let each thread process too large a chunk (thus needing more registers than are physically available for high occupancy), the compiler will “spill” variables to shared or local memory. This is slower.
